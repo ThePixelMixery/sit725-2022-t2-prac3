c3df69aa7dc7d6c99955d02bfd5aa177a00e07f9
Aya <pixelmixery@gmail.com>
2022-08-15 15:24:39 +1000

    Submission

--- server.js
+++ server.js
@@ -1,61 +1,11 @@
 var express = require("express");
 var app = express();
 
+// Add nav bar with logo, material box for image inspection, reveal card, modal (call it first) form, collect data from form timestamp 1:17:39 (-0:20:00)
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-const addNumbers = (number1, number2) => {
-  var num1 = parseInt(number1);
-  var num2 = parseInt(number2);
-  var result = num1 + num2;
-  return result;
-};
-
-const subNumbers = (number1, number2) => {
-  var num1 = parseInt(number1);
-  var num2 = parseInt(number2);
-  var result = num1 - num2;
-  return result;
-};
-const mulNumbers = (number1, number2) => {
-  var num1 = parseInt(number1);
-  var num2 = parseInt(number2);
-  var result = num1 * num2;
-  return result;
-};
-const divNumbers = (number1, number2) => {
-  var num1 = parseInt(number1);
-  var num2 = parseInt(number2);
-  var result = num1 / num2;
-  return result;
-};
-
-app.get("/addTwoNumbers", (req, res) => {
-  var number1 = req.query.number1;
-  var number2 = req.query.number2;
-  var result = addNumbers(number1, number2);
-  res.json({ statusCode: 200, data: result, message: "Success" });
-});
-
-app.get("/subTwoNumbers", (req, res) => {
-  var number1 = req.query.number1;
-  var number2 = req.query.number2;
-  var result = subNumbers(number1, number2);
-  res.json({ statusCode: 200, data: result, message: "Success" });
-});
-app.get("/mulTwoNumbers", (req, res) => {
-  var number1 = req.query.number1;
-  var number2 = req.query.number2;
-  var result = mulNumbers(number1, number2);
-  res.json({ statusCode: 200, data: result, message: "Success" });
-});
-app.get("/divTwoNumbers", (req, res) => {
-  var number1 = req.query.number1;
-  var number2 = req.query.number2;
-  var result = divNumbers(number1, number2);
-  res.json({ statusCode: 200, data: result, message: "Success" });
-});
 
 var port = process.env.port || 3000;
 app.listen(port, () => {
